1c0c791d3411f17101c2e7326abcaf8d1368fe1d
CristobalKri <cr.salcedo@duocuc.cl>
2025-12-02 11:09:31 -0300

    authControllerDoc

--- src/main/java/com/example/verduleria/controller/AuthController.java
+++ src/main/java/com/example/verduleria/controller/AuthController.java
@@ -1,8 +1,7 @@
 package com.example.verduleria.controller;
 
-import com.example.verduleria.model.Usuario;
-import com.example.verduleria.repository.UsuarioRepository;
-import com.example.verduleria.utils.JwtUtil;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,13 +10,23 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.example.verduleria.model.Usuario;
+import com.example.verduleria.repository.UsuarioRepository;
+import com.example.verduleria.utils.JwtUtil;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name="Manejo de Auth controller", description="Manejo de login y autenticacion cintinua de API sesion")
 public class AuthController {
 
     @Autowired
@@ -33,6 +42,14 @@ public class AuthController {
     private BCryptPasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
+    @Operation(summary = "API de autenticación login", responses = {
+            @ApiResponse(responseCode = "200", description = "Login exitoso"),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta o sintaxis invalida"),
+            @ApiResponse(responseCode = "401", description = "No autorizado o credenciales invalidas"),
+            @ApiResponse(responseCode = "404", description = "Recurso solicitado no encontrado"),
+            @ApiResponse(responseCode = "405", description = "Metodo no permitido"),
+            @ApiResponse(responseCode = "503", description = "Servicio no disponible")
+    })
     public Map<String, String> login(@RequestBody Map<String, String> body) {
 
         String username = body.get("username");
@@ -49,6 +66,12 @@ public class AuthController {
     }
 
     @GetMapping("/me")
+    @Operation(summary = "API de autenticación login", responses = {
+        @ApiResponse(responseCode = "200", description = "Autenticado"),
+        @ApiResponse(responseCode = "400", description = "Solicitud incorrecta o sintaxis invalida"),
+        @ApiResponse(responseCode = "401", description = "No autorizado o credenciales invalidas"),
+        @ApiResponse(responseCode = "403", description = "Acceso no permitido")
+    })
     public ResponseEntity<?> getCurrentUser(Authentication authentication) {
 
         Usuario user = usuarioRepository.findByUserName(authentication.getName())
