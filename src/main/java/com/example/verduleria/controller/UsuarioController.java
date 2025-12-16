08cd0ef88d5cab3b9abad93ba3d4ab986a72968e
federico-pereira <fe.pereira@duocuc.cl>
2025-12-16 08:31:41 -0300

    Actualizar UsuarioController.java

--- src/main/java/com/example/verduleria/controller/UsuarioController.java
+++ src/main/java/com/example/verduleria/controller/UsuarioController.java
@@ -25,7 +25,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/api/usuario")
+@RequestMapping("/api/usuarios")
 @Tag(name = "Usuario", description = "Operaciones usuario")
 public class UsuarioController {
 
